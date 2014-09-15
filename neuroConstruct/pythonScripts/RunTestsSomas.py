eacb50ef9b7acbc95d9899b90ae9e9b056184575
Padraig Gleeson <p.gleeson@gmail.com>
2014-09-15 13:28:36 +0100

    Using non var time step Nrn

--- neuroConstruct/pythonScripts/RunTestsSomas.py
+++ neuroConstruct/pythonScripts/RunTestsSomas.py
@@ -33,7 +33,7 @@ simConfigs = []
 
 simConfigs.append("TestSoma")
 
-simDt =                 0.0025
+simDt =                 0.001
 simDtOverride =         {"LEMS":0.0005}
 
 simulators =            ["NEURON", "GENESIS_PHYS", "GENESIS_SI", "MOOSE_PHYS", "MOOSE_SI", "LEMS"]
@@ -41,8 +41,7 @@ simulators =            ["NEURON", "GENESIS_SI", "MOOSE_PHYS", "MOOSE_SI", "LEMS
 
 numConcurrentSims =     4
 
-varTimestepNeuron =     True
-varTimestepTolerance =  0.00001
+varTimestepNeuron =     False
 
 plotSims =              True
 plotVoltageOnly =       True
@@ -70,8 +69,7 @@ def testAll(argv=None):
                                simDtOverride =        simDtOverride,
                                simulators =           simulators,
                                runInBackground =      runInBackground,
-                               varTimestepNeuron =    varTimestepNeuron,
-                               varTimestepTolerance = varTimestepTolerance)
+                               varTimestepNeuron =    varTimestepNeuron)
 
     simManager.reloadSims(plotVoltageOnly =   plotVoltageOnly,
                           plotSims =          plotSims,
@@ -82,8 +80,9 @@ def testAll(argv=None):
     spikeTimesToCheck = {'basket_soma_0': [109.942, 128.412, 146.881, 165.352, 183.823, 202.293, 220.763, 239.233, 257.703, 276.173, 294.643, 313.114, 331.584, 350.054, 368.524, 386.994, 405.464, 423.935, 442.404, 460.874, 479.345, 497.815, 516.284, 534.755, 553.226, 571.696, 590.167],
                          'pyramidal_soma_0': [102.484, 120.743, 148.434, 255.779, 330.06, 407.046, 483.384, 559.674]}
 
-    #spikeTimeAccuracy = 0.3
-    spikeTimeAccuracy = 1.6  # Large due to LEMS...
+
+    spikeTimeAccuracy = 0.31   # Works if no LEMS...
+    spikeTimeAccuracy = 1.6    # Large due to LEMS...
 
     report = simManager.checkSims(spikeTimesToCheck = spikeTimesToCheck,
                                   spikeTimeAccuracy = spikeTimeAccuracy)
